3e7523adbbd1d49082df69efcd535d7eaf0aa83e
Alex Walton <alex.w.walton@gmail.com>
2024-12-01 11:07:27 +0000

    feat(day-01): add solution for day 1 part 2

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -3,27 +3,35 @@ use std::iter;
 advent_of_code::solution!(1);
 
 fn parse_input(input: &str) -> (Vec<u32>, Vec<u32>) {
-    input.lines()
+    input
+        .lines()
         .map(|line| {
             let mut cols = line.split_whitespace();
-            (cols.next().unwrap().parse::<u32>().unwrap(), cols.next().unwrap().parse::<u32>().unwrap())
+            (
+                cols.next().unwrap().parse::<u32>().unwrap(),
+                cols.next().unwrap().parse::<u32>().unwrap(),
+            )
         })
         .unzip()
 }
 
 pub fn part_one(input: &str) -> Option<u32> {
     let (mut left, mut right) = parse_input(input);
-    
+
     left.sort();
     right.sort();
 
-    Some(iter::zip(left, right)
-        .map(|(l, r)| l.abs_diff(r))
-        .sum())
+    Some(iter::zip(left, right).map(|(l, r)| l.abs_diff(r)).sum())
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let (left, right) = parse_input(input);
+
+    Some(
+        left.iter()
+            .map(|n| n * right.iter().filter(|i| *i == n).count() as u32)
+            .sum(),
+    )
 }
 
 #[cfg(test)]
@@ -34,10 +42,10 @@ mod tests {
     fn test_parse_input() {
         let parsed_input = parse_input(&advent_of_code::template::read_file("examples", DAY));
 
-        assert_eq!(parsed_input, (
-            vec![3, 4, 2, 1, 3, 3],
-            vec![4, 3, 5, 3, 9, 3]
-        ))
+        assert_eq!(
+            parsed_input,
+            (vec![3, 4, 2, 1, 3, 3], vec![4, 3, 5, 3, 9, 3])
+        )
     }
 
     #[test]
@@ -49,6 +57,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(31));
     }
 }

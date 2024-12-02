a4df0507bbd97f2c849a1657290fc1ef1c181213
Alex Walton <alex.w.walton@gmail.com>
2024-12-02 08:35:09 +0000

    feat(day-02): add solution for day 2 part 1

---
+++ src/bin/02.rs
@@ -0,0 +1,81 @@
+advent_of_code::solution!(2);
+
+fn parse_input(input: &str) -> Vec<Vec<i32>> {
+    input
+        .lines()
+        .map(|line| {
+            line.split_whitespace()
+                .map(|n| n.parse::<i32>().unwrap())
+                .collect()
+        })
+        .collect()
+}
+
+fn are_levels_safe(levels: &Vec<i32>) -> bool {
+    let deltas = levels
+        .iter()
+        .enumerate()
+        .fold(vec![], |mut acc, (i, next)| {
+            if i > 0 {
+                acc.push(next - levels[i - 1]);
+            }
+            acc
+        });
+
+    let all_negative = deltas.iter().all(|l| *l < 0);
+    let all_positive = deltas.iter().all(|l| *l > 0);
+    let all_in_range = deltas.iter().all(|l| l.abs() <= 3);
+
+    (all_negative || all_positive) && all_in_range
+}
+
+pub fn part_one(input: &str) -> Option<u32> {
+    let levels = parse_input(input);
+
+    Some(levels.iter().filter(|l| are_levels_safe(*l)).count() as u32)
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    None
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parse_input() {
+        let input = &advent_of_code::template::read_file("examples", DAY);
+        assert_eq!(
+            parse_input(input),
+            vec![
+                vec![7, 6, 4, 2, 1],
+                vec![1, 2, 7, 8, 9],
+                vec![9, 7, 6, 2, 1],
+                vec![1, 3, 2, 4, 5],
+                vec![8, 6, 4, 4, 1],
+                vec![1, 3, 6, 7, 9],
+            ]
+        );
+    }
+
+    #[test]
+    fn test_are_levels_safe() {
+        assert_eq!(are_levels_safe(&vec![7, 6, 4, 2, 1]), true);
+        assert_eq!(are_levels_safe(&vec![1, 2, 7, 8, 9]), false);
+        assert_eq!(are_levels_safe(&vec![1, 3, 2, 4, 5]), false);
+        assert_eq!(are_levels_safe(&vec![1, 3, 6, 7, 9]), true);
+    }
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, None);
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, None);
+    }
+}
